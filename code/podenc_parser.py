881f036ec41dc52ad03e4fa3205f24b05607619b
hvgazula <hvgazula@umich.edu>
2021-03-09 01:30:11 -0500

    added back function to run encoding on significant electrodes

---
+++ code/podenc_parser.py
@@ -0,0 +1,49 @@
+import argparse
+
+
+def parse_arguments():
+    """Read commandline arguments
+
+    Returns:
+        Namespace: input as well as default arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--word-value',
+                        default='all',
+                        choices=['all', 'top', 'bottom'])
+    parser.add_argument('--window-size', type=int, default=200)
+    parser.add_argument('--stim', type=str, default='Podcast')
+    parser.add_argument('--pilot', type=str, default='')
+    parser.add_argument('--lags', nargs='+', type=int)
+    parser.add_argument('--output-prefix', type=str, default='')
+    parser.add_argument('--nonWords', action='store_true', default=False)
+    parser.add_argument('--datum-emb-fn',
+                        type=str,
+                        default='podcast-datum-glove-50d.csv')
+    parser.add_argument('--gpt2', type=int, default=1)
+    parser.add_argument('--bert', type=int, default=None)
+    parser.add_argument('--bart', type=int, default=None)
+    parser.add_argument('--glove', type=int, default=1)
+    parser.add_argument('--electrodes', nargs='*', type=int)
+    parser.add_argument('--npermutations', type=int, default=1)
+    parser.add_argument('--min-word-freq', nargs='?', type=int, default=1)
+    parser.add_argument('--job-id', type=int, default=0)
+    parser.add_argument('--output-parent-dir', type=str, default='test')
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--sid', nargs='?', type=int, default=None)
+    group.add_argument('--sig-elec-file', nargs='?', type=str, default=None)
+
+    group1 = parser.add_mutually_exclusive_group()
+    group1.add_argument('--shuffle', action='store_true', default=False)
+    group1.add_argument('--phase-shuffle', action='store_true', default=False)
+
+    args = parser.parse_args()
+
+    if not args.sid and args.electrodes:
+        parser.error("--electrodes requires --sid")
+
+    if not args.shuffle and not args.phase_shuffle:
+        args.npermutations = 1
+
+    return args
---
+++ code/utils.py
@@ -0,0 +1,46 @@
+import json
+import os
+import pickle
+from datetime import datetime
+
+
+def main_timer(func):
+    def function_wrapper():
+        start_time = datetime.now()
+        print(f'Start Time: {start_time.strftime("%A %m/%d/%Y %H:%M:%S")}')
+
+        func()
+
+        end_time = datetime.now()
+        print(f'End Time: {end_time.strftime("%A %m/%d/%Y %H:%M:%S")}')
+        print(f'Total runtime: {end_time - start_time} (HH:MM:SS)')
+
+    return function_wrapper
+
+
+def load_pickle(file):
+    """Load the datum pickle and returns as a dataframe
+
+    Args:
+        file (string): labels pickle from 247-decoding/tfs_pickling.py
+
+    Returns:
+        DataFrame: pickle contents returned as dataframe
+    """
+    with open(file, 'rb') as fh:
+        datum = pickle.load(fh)
+
+    return datum
+
+
+def write_config(dictionary):
+    """Write configuration to a file
+    Args:
+        CONFIG (dict): configuration
+    """
+    json_object = json.dumps(dictionary, sort_keys=True, indent=4)
+
+    config_file = os.path.join(os.path.join(os.getcwd(), 'results'),
+                               'config.json')
+    with open(config_file, "w") as outfile:
+        outfile.write(json_object)
